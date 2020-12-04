573e59ceaa3990efa4cc6723224a42e531c530fd
chent <chente31209@hotmail.com>
2020-12-03 22:14:59 -0600

    se agregaron el ejercio de la P(8) en vue

---
+++ Web/Unidad4/Ejercio 2/Prolleto/hello-vue3/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hi Visete Eduardo Martinez Morales " />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ Web/Unidad4/Ejercio 2/Prolleto/hello-vue3/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <p>18040071</p>
+  <button @click="count++">Aumentar</button>
+  <p>Numero Normal: {{count}} X2 {{count*2}} X3 {{count*3}} </p>
+  <button @click="count=0">Reinicar</button>
+  
+</template>
+
+<script>
+export default {
+  
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
